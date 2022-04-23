dbcf1ffa5cf4ce7750dcb0f41cce2c57952545c1
Max Chuquimia <max.chuquimia@users.noreply.github.io>
2022-04-23 14:29:43 +1000

    first commit

---
+++ OnAir/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  OnAir
+//
+//  Created by Max Chuquimia on 23/4/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
